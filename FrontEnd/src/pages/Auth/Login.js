bf100bf3bd395b5500711f0669fa01a982c4f5d8
Jamal U Din <jamaluddin14926@gmail.com>
2024-01-01 10:42:52 +0500

    login modified

--- FrontEnd/src/pages/Auth/Login.js
+++ FrontEnd/src/pages/Auth/Login.js
@@ -14,7 +14,7 @@ const Login = () => {
     const handleSubmit = async (e) => {
       e.preventDefault();
       try {
-        const res = await axios.post("/api/v1/auth/register",
+        const res = await axios.post("/api/v1/auth/login",
           { email, password }
         );
         if(res && res.data.success){
