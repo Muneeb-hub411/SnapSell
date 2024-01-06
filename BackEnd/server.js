250bd4f451d0993d619627a122bb772b2524a7f8
Jamal U Din <jamaluddin14926@gmail.com>
2024-01-06 17:09:16 +0500

    Proxy error resolved

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -17,7 +17,8 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-const port = process.env.PORT || 8080;
+// const port = process.env.PORT || 8080;
+const port = process.env.PORT;
 
 //routes
 app.use("/api/v1/auth", authRoutes);
