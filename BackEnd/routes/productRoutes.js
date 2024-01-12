b7be89f755fe53b784de2faae34564e44d88d432
Jamal U Din <jamaluddin14926@gmail.com>
2024-01-12 08:30:08 +0500

    Pagination and filters added

--- BackEnd/routes/productRoutes.js
+++ BackEnd/routes/productRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { isAdmin, requireSignIn } from "../middleware/authMiddleware.js";
-import { createProductController, deleteProductController, getAllProductsController, getSingleProductController, productImageController, updateProductController } from "../controller/productController.js";
+import { createProductController, deleteProductController, getAllProductsController, getSingleProductController, productCountController, productFiltersController, productImageController, productListController, updateProductController } from "../controller/productController.js";
 import formidable from "express-formidable";
 import { deleteCategoryCOntroller } from "../controller/categoryController.js";
 
@@ -32,5 +32,14 @@ router.delete('/delete-product/:pid', deleteProductController)
 
 // Update Product
 router.put("/update-product/:pid", requireSignIn, isAdmin, formidable(), updateProductController)
+
+// Filter product
+router.post("/product-filters", productFiltersController);
+
+// Product count
+router.get("/product-count", productCountController);
+
+// Product per page
+router.get("/product-list/:page", productListController);
 
 export default router;
---
+++ FrontEnd/src/components/Prices.js
@@ -0,0 +1,32 @@
+export const Prices = [
+    {
+      _id: 0,
+      name: "$0 to 19",
+      array: [0, 20],
+    },
+    {
+      _id: 1,
+      name: "$20 to 39",
+      array: [20, 40],
+    },
+    {
+      _id: 2,
+      name: "$40 to 59",
+      array: [40, 60],
+    },
+    {
+      _id: 3,
+      name: "$60 to 79",
+      array: [60, 80],
+    },
+    {
+      _id: 4,
+      name: "$80 to 99",
+      array: [80, 100],
+    },
+    {
+      _id: 5,
+      name: "$100 or more",
+      array: [100, 10000],
+    },
+  ];
