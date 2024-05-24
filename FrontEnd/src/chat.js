7e6fe18a509e5529876d2aa932a9538b5bfc2ff9
Muneeb-hub411 <muneebsaleem402@gmail.com>
2024-05-24 09:38:37 +0500

    chatbot integrated

--- FrontEnd/src/chat.js
+++ FrontEnd/src/chat.js
@@ -1,24 +1,28 @@
 import React, { Component } from "react";
 class KommunicateChat extends Component {
-constructor(props){
-super(props);
-}
-componentDidMount(){
-(function(d, m){
-var kommunicateSettings = {"appId":"1d1f2c1256c64aaed892fa068dc128cc9","popupWidget":true,"automaticChatOpenOnNavigation":true};
-var s = document.createElement("script"); s.type ="text/javascript"; s.async = true;
-s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
-var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    (function (d, m) {
+      var kommunicateSettings = {
+        appId: "86e9a0902eaec8c6aae69400c82d83fb",
+        popupWidget: true,
+        automaticChatOpenOnNavigation: true,
+      };
+      var s = document.createElement("script");
+      s.type = "text/javascript";
+      s.async = true;
+      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+      var h = document.getElementsByTagName("head")[0];
+      h.appendChild(s);
 
-window.kommunicate = m; m._globals = kommunicateSettings;
-})(document, window.kommunicate || {});
-}
-render(){
-return(
-<div>
-
-</div>
-)
-}
+      window.kommunicate = m;
+      m._globals = kommunicateSettings;
+    })(document, window.kommunicate || {});
+  }
+  render() {
+    return <div></div>;
+  }
 }
 export default KommunicateChat;
